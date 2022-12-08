4046950c740bbf7887bdc8580847a728e23cdbc1
aragorndp1812 <46215441+aragorndp1812@users.noreply.github.com>
2022-12-08 22:19:33 +0700

    Update android_phuho.js

--- android_phuho.js
+++ android_phuho.js
@@ -123,7 +123,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.418c7.js');
+        require('src/settings.7d06d.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.25d44.js');
@@ -131,8 +131,8 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.418c7.js');
-        require('src/cocos2d-jsb.c156d.js');
+        require('src/settings.7d06d.js');
+        require('src/cocos2d-jsb.fa53d.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.25d44.js');
         }
