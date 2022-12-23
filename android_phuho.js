6b1aaf0108003f235c3e368e310dbb72a333f064
aragorndp1812 <46215441+aragorndp1812@users.noreply.github.com>
2022-12-23 14:46:11 +0700

    Update android_phuho.js

--- android_phuho.js
+++ android_phuho.js
@@ -1,38 +1,58 @@
-
 window.boot = function () {
-    var settings = window._CCSettings;
-    window._CCSettings = undefined;
+    var settings = {
+					   "platform":"android",
+					   "groupList":[
+						  "default",
+						  "fish",
+						  "bullet"
+					   ],
+					   "collisionMatrix":[
+						  [
+							 true
+						  ],
+						  [
+							 false,
+							 false,
+							 true
+						  ],
+						  [
+							 false,
+							 true,
+							 false
+						  ]
+					   ],
+					   "hasResourcesBundle":false,
+					   "hasStartSceneBundle":false,
+					   "remoteBundles":[
+						  
+					   ],
+					   "subpackages":[
+						  
+					   ],
+					   "launchScene":"db://assets/android_phuho88/phuho88.fire",
+					   "orientation":"",
+					   "server":"https://dlph88.gadota.com",
+					   "jsList":[
+						  
+					   ],
+					   "bundleVers": {
+							"internal" : "627df"
+					   }
+					};
+		
+	window._CCSettings = undefined;
+	window.isHasSdkBox = false;
+	window.app_name = "ANDROID_PH88";
     var onProgress = null;
     
     var RESOURCES = cc.AssetManager.BuiltinBundleName.RESOURCES;
     var INTERNAL = cc.AssetManager.BuiltinBundleName.INTERNAL;
-    var MAIN = cc.AssetManager.BuiltinBundleName.MAIN;
-    function setLoadingDisplay () {
-        // Loading splash scene
-        var splash = document.getElementById('splash');
-        var progressBar = splash.querySelector('.progress-bar span');
-        onProgress = function (finish, total) {
-            var percent = 100 * finish / total;
-            if (progressBar) {
-                progressBar.style.width = percent.toFixed(2) + '%';
-            }
-        };
-        splash.style.display = 'block';
-        progressBar.style.width = '0%';
-
-        cc.director.once(cc.Director.EVENT_AFTER_SCENE_LAUNCH, function () {
-            splash.style.display = 'none';
-        });
-    }
-
+    var MAIN = "https://dlph88.gadota.com/main";
+	
     var onStart = function () {
 
         cc.view.enableRetina(true);
         cc.view.resizeWithBrowserSize(true);
-
-        if (cc.sys.isBrowser) {
-            setLoadingDisplay();
-        }
 
         if (cc.sys.isMobile) {
             if (settings.orientation === 'landscape') {
@@ -52,37 +72,50 @@ window.boot = function () {
             ].indexOf(cc.sys.browserType) < 0);
         }
 
-        // Limit downloading max concurrent task to 2,
-        // more tasks simultaneously may cause performance draw back on some android system / browsers.
-        // You can adjust the number based on your own test result, you have to set it before any loading process to take effect.
+
         if (cc.sys.isBrowser && cc.sys.os === cc.sys.OS_ANDROID) {
-            cc.assetManager.downloader.maxConcurrency = 2;
-            cc.assetManager.downloader.maxRequestsPerFrame = 2;
+            cc.assetManager.downloader.maxConcurrency = 20;
+            cc.assetManager.downloader.maxRequestsPerFrame = 10;
         }
 
-        var launchScene = settings.launchScene;
-        var bundle = cc.assetManager.bundles.find(function (b) {
-            return b.getSceneInfo(launchScene);
-        });
-        
-        bundle.loadScene(launchScene, null, onProgress,
-            function (err, scene) {
-                if (!err) {
-                    cc.director.runSceneImmediate(scene);
-                    if (cc.sys.isBrowser) {
-                        // show canvas
-                        var canvas = document.getElementById('GameCanvas');
-                        canvas.style.visibility = '';
-                        var div = document.getElementById('GameDiv');
-                        if (div) {
-                            div.style.backgroundImage = '';
-                        }
-                        console.log('Success to load scene: ' + launchScene);
-                    }
-                }
-            }
-        );
+		var scene = new cc.Scene("loadingScene");
+		var canvasNode = new cc.Node("Canvas");
+		var canvasComp = canvasNode.addComponent(cc.Canvas);
 
+		var label = new cc.Node("Loading");
+		var labelComp = label.addComponent(cc.Label);
+		labelComp.string = "Loading...";
+		label.color = cc.Color.WHITE;
+		canvasNode.addChild(label);
+		
+		scene.addChild(canvasNode);
+		
+		var launchScene = settings.launchScene;
+
+		cc.director.runSceneImmediate(scene, () => {
+			cc.assetManager.loadBundle(MAIN,
+			function (err) {
+				var bundle = cc.assetManager.bundles.find(function (b) {
+					return b.getSceneInfo(launchScene);
+				});
+				var lastProgress = 0;
+				bundle.preloadScene(launchScene, (loaded, total) => {
+						var prog = loaded / total;
+						if(lastProgress > prog)
+							return;
+						lastProgress = prog;
+						labelComp.string = Math.floor(lastProgress * 100) + "%";
+					}, (err) => {
+						if(err)
+						{
+							labelComp.string = "Please try again";
+							onStart();
+							return;
+						}
+						cc.director.loadScene(launchScene);
+					});
+			});
+		});
     };
 
     var option = {
@@ -99,26 +132,27 @@ window.boot = function () {
         remoteBundles: settings.remoteBundles,
         server: settings.server
     });
-    
-    var bundleRoot = [INTERNAL];
+	
+
+	var bundleRoot = [INTERNAL];
     settings.hasResourcesBundle && bundleRoot.push(RESOURCES);
 
     var count = 0;
+	
     function cb (err) {
         if (err) return console.error(err.message, err.stack);
         count++;
         if (count === bundleRoot.length + 1) {
-            cc.assetManager.loadBundle(MAIN, function (err) {
-                if (!err) cc.game.run(option, onStart);
-            });
+            cc.game.run(option, onStart);
         }
-    }
+     }
 
-    cc.assetManager.loadScript(settings.jsList.map(function (x) { return 'src/' + x;}), cb);
+     cc.assetManager.loadScript(settings.jsList.map(function (x) { return 'src/' + x;}), cb);
 
-    for (var i = 0; i < bundleRoot.length; i++) {
-        cc.assetManager.loadBundle(bundleRoot[i], cb);
-    }
+     for (var i = 0; i < bundleRoot.length; i++) {
+         cc.assetManager.loadBundle(bundleRoot[i], cb);
+     }
+
 };
 
 if (window.jsb) {
